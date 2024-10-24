d2328b9c8c1151c0b48e426f2af61ed0febe74b5
jfryy <fotherby1@gmail.com>
2024-10-24 09:41:30 -0700

    remaps for buffer telescope

--- lua/mymachine/init.lua
+++ lua/mymachine/init.lua
@@ -189,13 +189,19 @@ require("lazy").setup({
         opts = {},
         dependencies = { 'nvim-treesitter/nvim-treesitter' }
     },
+    --    {
+    --        dir = "~/.config/nvim/lua/codeposse/",
+    --        config = function()
+    --            require("codeposse").setup({
+    --                -- Add any setup options or configuration here
+    --            })
+    --        end
+    --    },
     {
-        dir = "~/.config/nvim/lua/localpilot/",
-        config = function()
-            require("localpilot").setup({
-                -- Add any setup options or configuration here
-            })
-        end
+        'altermo/ultimate-autopair.nvim',
+        event = { 'InsertEnter', 'CmdlineEnter' },
+        branch = 'v0.6', --recommended as each new version will have breaking changes
+        opts = {}
     },
     {
         "epwalsh/obsidian.nvim",
--- lua/mymachine/remap.lua
+++ lua/mymachine/remap.lua
@@ -12,9 +12,15 @@ vim.cmd('command Q q')
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>r', builtin.command_history, {})
+vim.keymap.set('n', '<leader>rr', builtin.command_history, {})
 vim.keymap.set('n','<leader>gg', builtin.git_status, {})
 vim.keymap.set('n','<leader>gc', builtin.git_commits, {})
+vim.keymap.set('n','<leader>bb', builtin.buffers, {})
+
+-- REPLACE ALL helper:
+-- Find string in all files using Telescope and populate quick fix list using
+-- ctrl + q
+-- :cdo %s/oldstring/newstring/g | update
 
 -- nvim-tree
 vim.api.nvim_set_keymap('n', '<C-n>', ':Neotree toggle<CR>', { noremap = true, silent = true })
@@ -23,5 +29,5 @@ vim.api.nvim_set_keymap('n', '<C-n>', ':Neotree toggle<CR>', { noremap = true, s
 vim.api.nvim_set_keymap('n', '<leader>tt', ':ToggleTerm dir=git_dir size=20 <CR>', { noremap = true, silent = true })
 
 -- local pilot
-vim.api.nvim_set_keymap('v', '<leader>ll', ':<C-U>LocalPilotGenVisual<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>ll', ':<C-U>LocalPilotGenWin<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('v', '<leader>ll', ':<C-U>CodePosseGenVisual<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>ll', ':<C-U>CodePosseGenWin<CR>', { noremap = true, silent = true })
